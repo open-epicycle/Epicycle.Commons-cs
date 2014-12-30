983647bb200abf327ddf7c28f231ebc8f7ed20d1
untrots <untrots@gmail.com>
2014-12-30 16:14:42 +0200

    Creating a generic ReportingStopwatch that works with both IStatisticsReporter and INumericReport

--- projects/Epicycle.Commons_cs/Reporting/IStatisticsReporter.cs
+++ projects/Epicycle.Commons_cs/Reporting/IStatisticsReporter.cs
@@ -1,18 +1,11 @@
-﻿using System;
-
-namespace Epicycle.Commons.Reporting
+﻿namespace Epicycle.Commons.Reporting
 {
-    public interface IStatisticsReporter
+    public interface IStatisticsReporter : INumericReport
     {
         IStatisticsReporter CreateSubReporter(string name);
 
         void ReportEvent(string name);
         void ReportEvent(string name, int amount);
-
-        void ReportParameter(string name, int value);
-        void ReportParameter(string name, double value);
-
-        IDisposable Time(string name);
 
         void Report(IReport report);
     }
---
+++ projects/Epicycle.Commons_cs/Reporting/ReportingStopwatch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Epicycle.Commons.Reporting
+{
+    public sealed class ReportingStopwatch : IDisposable
+    {
+        private INumericReport _report;
+        private string _name;
+        private System.Diagnostics.Stopwatch _stopwatch;
+
+        public ReportingStopwatch(INumericReport report, string name)
+        {
+            _report = report;
+            _name = name;
+
+            _stopwatch = new System.Diagnostics.Stopwatch();
+            _stopwatch.Start();
+        }
+
+        public void Dispose()
+        {
+            _stopwatch.Stop();
+
+            var dt_sec = ((double)_stopwatch.ElapsedTicks) / System.Diagnostics.Stopwatch.Frequency;
+
+            _report.Report(_name, dt_sec);
+        }
+    }
+}
