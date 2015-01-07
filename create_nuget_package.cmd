36b841891f7ac1d8475326cf0ca6bd62fbf6bf12
Dima Potekhin <untrots@gmail.com>
2015-01-07 14:50:22 +0200

    Adding an empty Epicycle.Commons.TestUtils_cs project

--- create_nuget_package.cmd
+++ create_nuget_package.cmd
@@ -9,12 +9,21 @@ copy package.nuspec NuGetPackage\Epicycle.Commons-cs.0.1.3.0\Epicycle.Commons-cs
 copy README.md NuGetPackage\Epicycle.Commons-cs.0.1.3.0\README.md
 copy LICENSE NuGetPackage\Epicycle.Commons-cs.0.1.3.0\LICENSE
 
+xcopy bin\net35\Release\Epicycle.Commons.TestUtils_cs.dll NuGetPackage\Epicycle.Commons-cs.0.1.3.0\lib\net35\
+xcopy bin\net35\Release\Epicycle.Commons.TestUtils_cs.pdb NuGetPackage\Epicycle.Commons-cs.0.1.3.0\lib\net35\
+xcopy bin\net35\Release\Epicycle.Commons.TestUtils_cs.xml NuGetPackage\Epicycle.Commons-cs.0.1.3.0\lib\net35\
 xcopy bin\net35\Release\Epicycle.Commons_cs.dll NuGetPackage\Epicycle.Commons-cs.0.1.3.0\lib\net35\
 xcopy bin\net35\Release\Epicycle.Commons_cs.pdb NuGetPackage\Epicycle.Commons-cs.0.1.3.0\lib\net35\
 xcopy bin\net35\Release\Epicycle.Commons_cs.xml NuGetPackage\Epicycle.Commons-cs.0.1.3.0\lib\net35\
+xcopy bin\net40\Release\Epicycle.Commons.TestUtils_cs.dll NuGetPackage\Epicycle.Commons-cs.0.1.3.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Commons.TestUtils_cs.pdb NuGetPackage\Epicycle.Commons-cs.0.1.3.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Commons.TestUtils_cs.xml NuGetPackage\Epicycle.Commons-cs.0.1.3.0\lib\net40\
 xcopy bin\net40\Release\Epicycle.Commons_cs.dll NuGetPackage\Epicycle.Commons-cs.0.1.3.0\lib\net40\
 xcopy bin\net40\Release\Epicycle.Commons_cs.pdb NuGetPackage\Epicycle.Commons-cs.0.1.3.0\lib\net40\
 xcopy bin\net40\Release\Epicycle.Commons_cs.xml NuGetPackage\Epicycle.Commons-cs.0.1.3.0\lib\net40\
+xcopy bin\net45\Release\Epicycle.Commons.TestUtils_cs.dll NuGetPackage\Epicycle.Commons-cs.0.1.3.0\lib\net45\
+xcopy bin\net45\Release\Epicycle.Commons.TestUtils_cs.pdb NuGetPackage\Epicycle.Commons-cs.0.1.3.0\lib\net45\
+xcopy bin\net45\Release\Epicycle.Commons.TestUtils_cs.xml NuGetPackage\Epicycle.Commons-cs.0.1.3.0\lib\net45\
 xcopy bin\net45\Release\Epicycle.Commons_cs.dll NuGetPackage\Epicycle.Commons-cs.0.1.3.0\lib\net45\
 xcopy bin\net45\Release\Epicycle.Commons_cs.pdb NuGetPackage\Epicycle.Commons-cs.0.1.3.0\lib\net45\
 xcopy bin\net45\Release\Epicycle.Commons_cs.xml NuGetPackage\Epicycle.Commons-cs.0.1.3.0\lib\net45\
