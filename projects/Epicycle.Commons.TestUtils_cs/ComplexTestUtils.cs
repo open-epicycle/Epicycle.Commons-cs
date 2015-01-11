e8504de16b65cdba6f531cf1889cbeb6c51d2a72
untrots <untrots@gmail.com>
2015-01-11 16:04:18 +0200

    TestUtils: +NumericTestUtils; +ComplexTestUtils

---
+++ projects/Epicycle.Commons.TestUtils_cs/ComplexTestUtils.cs
@@ -0,0 +1,30 @@
+﻿// [[[[INFO>
+// Copyright 2015 Epicycle (http://epicycle.org, https://github.com/open-epicycle)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// For more information check https://github.com/open-epicycle/Epicycle.Commons-cs
+// ]]]]
+
+using System.Numerics;
+
+namespace Epicycle.Commons.TestUtils
+{
+    public static class ComplexTestUtils
+    {
+        public static bool AreEqual(Complex c1, Complex c2, double eplsilon = NumericTestUtils.Epsilon)
+        {
+            return NumericTestUtils.AreEqual(c1.Real, c2.Real) && NumericTestUtils.AreEqual(c1.Imaginary, c2.Imaginary);
+        }
+    }
+}
