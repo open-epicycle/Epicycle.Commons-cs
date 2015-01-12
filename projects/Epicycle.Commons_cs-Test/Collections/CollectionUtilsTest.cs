ca3b866e793f440c52963e6d4e9db564181fd5a6
untrots <untrots@gmail.com>
2015-01-12 12:41:44 +0200

    CollectionUtilsTest: Full tests

--- projects/Epicycle.Commons_cs-Test/Collections/CollectionUtilsTest.cs
+++ projects/Epicycle.Commons_cs-Test/Collections/CollectionUtilsTest.cs
@@ -25,6 +25,90 @@ namespace Epicycle.Commons.Collections
     [TestFixture]
     public sealed class CollectionUtilsTest : AssertionHelper
     {
+        private List<int> _list;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _list = new List<int> { 1, 2, 3 };
+        }
+
+        private void AssertListEquals<T>(IList<T> expected, IReadOnlyList<T> list)
+        {
+            Assert.AreEqual(expected.Count, list.Count);
+            for (var i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i], list[i]);
+            }
+        }
+
+        private void AssertListEquals<T>(IList<T> expected, IList<T> list)
+        {
+            Assert.AreEqual(expected.Count, list.Count);
+            for (var i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i], list[i]);
+            }
+        }
+
+        [Test]
+        public void AsReadOnlyList_resulting_list_is_the_same()
+        {
+            AssertListEquals(_list, _list.AsReadOnlyList());
+        }
+
+        [Test]
+        public void Last_returns_last_element_of_a_list()
+        {
+            Assert.AreEqual(_list[_list.Count - 1], _list.Last());
+        }
+
+        [Test]
+        public void RemoveLast_removes_last_element_from_list()
+        {
+            _list.RemoveLast();
+            AssertListEquals(new List<int> { 1, 2 }, (IList<int>)_list);
+        }
+
+        [Test]
+        public void ElementAtCyclic_large_negative_to_large_positive_indices_give_correct_element()
+        {
+            for(var i = -100; i < 100; i++)
+            {
+                Assert.AreEqual(_list[i.Mod(_list.Count)], _list.AsReadOnlyList().ElementAtCyclic(i));
+            }
+        }
+
+        [Test]
+        public void AsSingleton_wraps_item_with_a_single_element_list()
+        {
+            AssertListEquals(new List<int> { 123 }, (123).AsSingleton());
+        }
+
+        [Test]
+        public void Reverse_returns_reversed_list()
+        {
+            AssertListEquals(new List<int> { 3, 2, 1 }, _list.AsReadOnlyList().Reverse());
+        }
+
+        [Test]
+        public void AsReverse_returns_reversed_list()
+        {
+            AssertListEquals(new List<int> { 3, 2, 1 }, _list.AsReadOnlyList().AsReverse());
+        }
+
+        [Test]
+        public void AsCyclicPermutation_positive_shift_gives_correct_permutation()
+        {
+            AssertListEquals(new List<int> { 2, 3, 1 }, _list.AsReadOnlyList().AsCyclicPermutation(2));
+        }
+
+        [Test]
+        public void AsCyclicPermutation_negative_shift_gives_correct_permutation()
+        {
+            AssertListEquals(new List<int> { 3, 1, 2 }, _list.AsReadOnlyList().AsCyclicPermutation(-2));
+        }
+
         [Test]
         public void SieveByIndex_adds_list_elements_at_received_indices_to_one_collection_and_other_elements_to_another_collection()
         {
