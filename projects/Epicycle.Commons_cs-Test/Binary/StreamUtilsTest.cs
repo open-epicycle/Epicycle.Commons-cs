e8516c4f2fabff2c2c32cf1e6b41ef47456f35f6
untrots <untrots@gmail.com>
2015-03-05 14:57:51 +0200

    +StreamUtils

---
+++ projects/Epicycle.Commons_cs-Test/Binary/StreamUtilsTest.cs
@@ -0,0 +1,147 @@
+﻿// [[[[INFO>
+// Copyright 2015 Epicycle (http://epicycle.org, https://github.com/open-epicycle)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// For more information check https://github.com/open-epicycle/Epicycle.Commons-cs
+// ]]]]
+
+using NUnit.Framework;
+using System.IO;
+
+namespace Epicycle.Commons.Binary
+{
+    [TestFixture]
+    public class StreamUtilsTest
+    {
+        private Stream _stream;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var data = new byte[] { 1, 2, 3, 4, 5 };
+
+            _stream = new MemoryStream(data);
+        }
+
+        private void MoveTo(int newPosition)
+        {
+            _stream.Seek(newPosition, SeekOrigin.Begin);
+        }
+
+        [Test]
+        public void HasEnded_zero_position_return_false()
+        {
+            Assert.That(_stream.HasEnded(), Is.False);
+        }
+
+        [Test]
+        public void HasEnded_last_position_return_false()
+        {
+            MoveTo(4);
+            Assert.That(_stream.HasEnded(), Is.False);
+        }
+
+        [Test]
+        public void HasEnded_just_after_end_position_return_false()
+        {
+            MoveTo(5);
+            Assert.That(_stream.HasEnded(), Is.True);
+        }
+
+        [Test]
+        public void HasEnded_way_over_end_position_return_false()
+        {
+            MoveTo(15);
+            Assert.That(_stream.HasEnded(), Is.True);
+        }
+
+        [Test]
+        public void BytesLeft_different_positions_return_correct_answer()
+        {
+            for(var i = 0; i < 5; i++)
+            {
+                MoveTo(i);
+                Assert.That(_stream.BytesLeft(), Is.EqualTo(5 - i));
+            }
+        }
+
+        [Test]
+        public void BytesLeft_after_the_end_position_returns_zero()
+        {
+            MoveTo(25);
+            Assert.That(_stream.BytesLeft(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void AssertNotEnded_if_not_ended_nothing_happens()
+        {
+            MoveTo(3);
+            _stream.AssertNotEnded();
+        }
+
+        [Test]
+        [ExpectedException(typeof(EndOfStreamException))]
+        public void AssertNotEnded_if_ended_exception_is_thrown()
+        {
+            MoveTo(15);
+            _stream.AssertNotEnded();
+        }
+
+        [Test]
+        public void AssertBytesLeft_exactly_bytes_left_does_nothing()
+        {
+            MoveTo(3);
+            _stream.AssertBytesLeft(2);
+        }
+
+        [Test]
+        public void AssertBytesLeft_more_than_needed_bytes_left_does_nothing()
+        {
+            MoveTo(1);
+            _stream.AssertBytesLeft(2);
+        }
+
+        [Test]
+        [ExpectedException(typeof(EndOfStreamException))]
+        public void AssertNotEnded_not_enough_bytes_exception_is_thrown()
+        {
+            MoveTo(3);
+            _stream.AssertBytesLeft(3);
+        }
+
+        [Test]
+        public void Skip_zero_delta_does_nothing()
+        {
+            MoveTo(1);
+            _stream.Skip(0);
+            Assert.That(_stream.Position, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Skip_positive_delta_moves_forward()
+        {
+            MoveTo(1);
+            _stream.Skip(2);
+            Assert.That(_stream.Position, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Skip_negative_delta_moves_backward()
+        {
+            MoveTo(3);
+            _stream.Skip(-2);
+            Assert.That(_stream.Position, Is.EqualTo(1));
+        }
+    }
+}
---
+++ projects/Epicycle.Commons_cs/Binary/StreamUtils.cs
@@ -0,0 +1,68 @@
+﻿// [[[[INFO>
+// Copyright 2015 Epicycle (http://epicycle.org, https://github.com/open-epicycle)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// For more information check https://github.com/open-epicycle/Epicycle.Commons-cs
+// ]]]]
+
+using System;
+using System.IO;
+
+namespace Epicycle.Commons.Binary
+{
+    public static class StreamUtils
+    {
+        public static bool HasEnded(this Stream @this)
+        {
+            return @this.Position >= @this.Length;
+        }
+
+        public static long BytesLeft(this Stream @this)
+        {
+            return Math.Max(@this.Length - @this.Position, 0);
+        }
+
+        /// <summary>
+        /// Validates that the stream has not ended.
+        /// </summary>
+        /// <param name="this">The stream.</param>
+        /// <exception cref="System.IO.EndOfStreamException"> Throws if the validation fails.</exception>
+        public static void AssertNotEnded(this Stream @this)
+        {
+            if (@this.HasEnded())
+            {
+                throw new EndOfStreamException("The stream has ended!");
+            }
+        }
+
+        public static void AssertBytesLeft(this Stream @this, long minBytes)
+        {
+            if (@this.BytesLeft() < minBytes)
+            {
+                throw new EndOfStreamException("Not enough bytes in stream!");
+            }
+        }
+
+        /// <summary>
+        /// Skips the given <paramref name="amount"/> of bytes from the current position. If the new position is beyond
+        /// beyond the end of the stream, no exception will be thrown.
+        /// </summary>
+        /// <param name="this">The stream.</param>
+        /// <param name="amount">The number of bytes to skip. Negative amount will move the cursor backwards.</param>
+        public static void Skip(this Stream @this, long amount)
+        {
+            @this.Seek(amount, SeekOrigin.Current);
+        }
+    }
+}
