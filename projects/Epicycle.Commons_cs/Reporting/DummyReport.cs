10bd6aa3157f18a89f3c616d9b79cebeb7c55f5b
untrots <untrots@gmail.com>
2015-07-13 13:41:05 +0300

    +DummyReport

---
+++ projects/Epicycle.Commons_cs/Reporting/DummyReport.cs
@@ -0,0 +1,44 @@
+﻿// [[[[INFO>
+// Copyright 2015 Epicycle (http://epicycle.org, https://github.com/open-epicycle)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// For more information check https://github.com/open-epicycle/Epicycle.Commons-cs
+// ]]]]
+
+namespace Epicycle.Commons.Reporting
+{
+    public sealed class DummyReport : IReport
+    {
+        public static readonly DummyReport Instance = new DummyReport();
+
+        private DummyReport() { }
+
+        public IReport SubReport(string name)
+        {
+            return Instance;
+        }
+
+        public void Report(string name, int value) { }
+
+        public void Report(string name, long value) { }
+
+        public void Report(string name, float value) { }
+
+        public void Report(string name, double value) { }
+
+        public void Report(string name, string value) { }
+
+        public void Report(string name, object value) { }
+    }
+}
