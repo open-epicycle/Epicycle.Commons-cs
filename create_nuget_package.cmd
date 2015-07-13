32c77b8a90aae3dc9225e732341fa2405ba88c91
untrots <untrots@gmail.com>
2015-07-13 14:39:52 +0300

    Regenerating create_nuget_package.cmd

--- create_nuget_package.cmd
+++ create_nuget_package.cmd
@@ -28,4 +28,11 @@ xcopy bin\net45\Release\Epicycle.Commons_cs.dll NuGetPackage\Epicycle.Commons-cs
 xcopy bin\net45\Release\Epicycle.Commons_cs.pdb NuGetPackage\Epicycle.Commons-cs.0.1.7.0\lib\net45\
 xcopy bin\net45\Release\Epicycle.Commons_cs.xml NuGetPackage\Epicycle.Commons-cs.0.1.7.0\lib\net45\
 
+cd NuGetPackage
+nuget pack Epicycle.Commons-cs.0.1.7.0\Epicycle.Commons-cs.0.1.7.0.nuspec -Properties version=0.1.7.0
+7z a -tzip Epicycle.Commons-cs.0.1.7.0.zip Epicycle.Commons-cs.0.1.7.0 Epicycle.Commons-cs.0.1.7.0.nupkg
+
+echo nuget push Epicycle.Commons-cs.0.1.7.0.nupkg > push.cmd
+echo pause >> push.cmd
+
 pause
